c4c6ad9e22a07466ad4256cbcf3bef8cbf2eae31
CodeWizardette <131625021+CodeWizardette@users.noreply.github.com>
2023-06-10 13:00:52 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+"""
+بسم الله الرحمن الرحيم
+"""
 import matplotlib.pyplot as plt
 from collections import Counter
+import string
 
 def ebcet_hesapla(kelime):
     ebcet_tablosu = {
@@ -22,7 +26,9 @@ def metin_temizle(metin):
 
 def kelime_frekansi(metin):
     kelimeler = metin.split()
-    frekanslar = Counter(kelimeler)
+    frekanslar=defaultdict(int)
+    for kelime in kelimeler
+        frekanslar[kelime] +=1
     return frekanslar
 
 def kelime_uzunlugu_histogrami(metin):
@@ -44,11 +50,39 @@ def kelime_freksansi_grafik(metin, en_cok=10):
     plt.title(f'En Sık {en_cok} Kelime')
     plt.xticks(rotation=45)
     plt.show()
+    
+    
+    
+with open("kuran.txt", "r", encoding="utf-8") as file:
+    kuran_metni = file.read()7
 
 kuran_metni = """
 Buraya Kur'an metnini ekleyin.
 """
+temiz_metin = metin_temizle(kuran_metni)
+
+# Stop kelimeleri çıkar
+nltk.download('stopwords')
+stop_kelimeler = set(stopwords.words('turkish'))
+kelimeler = temiz_metin.split()
+kelimeler_filtrli = [kelime for kelime in kelimeler if kelime not in stop_kelimeler]
+
+kelime_uzunlugu_histogrami(kelimeler_filtrli)
+frekanslar = kelime_frekansi(kelimeler_filtrli)
+kelime_freksansi_grafik(frekanslar)
+
+# Bigram frekans analizi
+bigramler = ngrams(kelimeler_filtrli, 2)
+bigram_frekanslar = defaultdict(int)
+for bigram in bigramler:
+    bigram_frekanslar[bigram] += 1
+
+kelime_freksansi_grafik(bigram_frekanslar, en_cok=10)
 
 temiz_metin = metin_temizle(kuran_metni)
 kelime_uzunlugu_histogrami(temiz_metin)
 kelime_freksansi_grafik(temiz_metin)
+
+# Grafik gösterme yerine kaydetme
+plt.savefig("kelime_uzunlugu_histogrami.png")
+plt
